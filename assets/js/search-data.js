2b6c0c5be2329cac6d27726980d62beb06c61eaa
Minseok5OMS <harrymark0@dgist.ac.kr>
2026-03-12 10:55:21 +0000

    Deploying to gh-pages from @ Minseok5OMS/Minseok5OMS.github.io@096387f3d32df853f66b0689b938153fb3b53047 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,8 +448,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-a-paper-accepted-at-cvpr-2026-findings-sparkles-smile",
+          title: 'A paper accepted at CVPR 2026 (Findings)! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
